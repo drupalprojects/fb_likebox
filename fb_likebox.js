8fd9fc4adb3b079e7d3bd2310f77e9864f81f4c7
baekelandt <edward_baekelandt@hotmail.com>
2015-06-07 00:53:32 +0200

    Issue #2499463: Invalid App Id -> added AppID: 123

--- fb_likebox.js
+++ fb_likebox.js
@@ -4,7 +4,7 @@
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.3";
+	  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.3&appId=123";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
